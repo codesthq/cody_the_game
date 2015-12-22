34b6c7544d47baf67152838fe863aa6a4ffa7200
Bartosz Zurkowski <bartosz.zurkowski@codesthq.com>
2015-12-22 10:41:54 +0100

    Add Conversation model

--- db/schema.rb
+++ db/schema.rb
@@ -22,4 +22,9 @@ ActiveRecord::Schema.define(version: 20151222093244) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "conversations", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
