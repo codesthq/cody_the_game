7cb8c5bd5915a1c2f79d0cbac4eebfd13b0efd4d
fanfilmu <michal.begejowicz@codesthq.com>
2015-12-22 14:08:06 +0100

    Add submission actions in APIClient

--- app/assets/javascripts/services/api_client.js
+++ app/assets/javascripts/services/api_client.js
@@ -3,7 +3,9 @@
     if (typeof host == 'undefined') throw "You must pass host parameter!";
 
     var levels_path = host + '/levels';
-    var level_path = function(id) { return level_path + '/' + id; };
+    var level_path = function(id) { return levels_path + '/' + id; };
+    var submissions_path = host + '/submissions';
+    var submission_path = function(id) { return submissions_path + '/' + id; };
 
     // List available levels
     // PARAMS:
@@ -21,11 +23,37 @@
     // PARAMS:
     //  levelName - name of the level
     //  callback - function to be called with retrieved data
-    //   error - function to be called when an error occurs
+    //  error - function to be called when an error occurs
     // data format:
     //
     this.getLevel = function(id, callback, error) {
       $.get(level_path(id))
+        .done(callback)
+        .fail(error);
+    }
+
+    // Get data about specific level
+    // PARAMS:
+    //  code - code to be submitted
+    //  callback - function to be called with retrieved data
+    //   error - function to be called when an error occurs
+    // data format:
+    //
+    this.submitCode = function(code, callback, error) {
+      $.post(submissions_path, { submission: { content: code } })
+        .done(callback)
+        .fail(error);
+    }
+
+    // Get submission status
+    // PARAMS:
+    //  id - id of the submission
+    //  callback - function to be called with retrieved data
+    //  error - function to be called when an error occurs
+    // data format:
+    //
+    this.getSubmission = function(id, callback, error) {
+      $.get(submission_path(id))
         .done(callback)
         .fail(error);
     }
