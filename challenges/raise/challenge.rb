38f63a8d159484783222b4c0a3783a988c4427c3
Michał Krężelewski <michal.krezelewski@codesthq.com>
2015-12-22 14:16:26 +0100

    Add raise challenge

---
+++ challenges/raise/challenge.rb
@@ -0,0 +1,13 @@
+# After executing your code we will open class Foo and add method
+
+def method_name
+  raise "I won"
+end
+
+# write class in a way, that method_name will not raise an exception
+
+class Foo
+  #solution
+  # def raise(argument)
+  # end
+end
