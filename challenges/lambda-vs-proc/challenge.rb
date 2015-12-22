54fcceb5a149a79ba5df7dbbc0e79d8457231466
Michał Krężelewski <michal.krezelewski@codesthq.com>
2015-12-22 13:36:08 +0100

    Add lambda vs proc challenge

---
+++ challenges/lambda-vs-proc/challenge.rb
@@ -0,0 +1,25 @@
+def array
+  ["def", "ruby", "do", "if", "end"]
+end
+
+def response_formatter(finder)
+  puts "First two character element in array is \"#{finder.call}\""
+end
+
+def greeter
+  puts "Hello"
+
+  finder = Proc.new { array.each { |element| return element if element.length == 2} }
+  # simplest solution
+  # finder = lambda { array.each { |element| return element if element.length == 2} }
+
+
+  response_formatter(finder)
+end
+
+greeter
+
+# change body of method greeter so it always return message:
+# Hello
+# First two character element in array is "X"
+# Where X is first two letter string in array
