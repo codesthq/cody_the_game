d7229058169fed79bf95b26786a6fb7c98b87e80
rapsad <dawid.polchlopek@codesthq.com>
2015-12-22 11:10:19 +0100

    Remove method

--- app/controllers/api/v1/levels_controller.rb
+++ app/controllers/api/v1/levels_controller.rb
@@ -2,8 +2,4 @@ class API::V1::LevelsController < API::V1::BaseController
   def index
     render json: { message: "Another" }, status: 200
   end
-
-  def validate
-    render json: { message: "Super code!" }, status: 200
-  end
 end
