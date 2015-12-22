9e9a03ca034645ad58c08aeb155f660327bf1db0
rapsad <dawid.polchlopek@codesthq.com>
2015-12-22 14:51:22 +0100

    Changes in auth

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -4,14 +4,20 @@ class API::BaseController < ApplicationController
   private
 
   def current_session
+
     @current_session ||= begin
       if cookies[:cody_user].present?
-        GameSession.find_by cookie_key: cookies[:cody_user]
+        game_session = GameSession.find_by cookie_key: cookies[:cody_user]
+        game_session ? game_session : create_game_session
       else
-        enc = SecureRandom.base64(24)
-        cookies[:cody_user] = enc
-        GameSession.create cookie_key: enc
+        create_game_session
       end
     end
+  end
+
+  def create_game_session
+    enc = SecureRandom.base64(24)
+    cookies[:cody_user] = enc
+    GameSession.create cookie_key: enc
   end
 end
