25a3f5cecb62831ba89113d16257308c39616dc6
rapsad <dawid.polchlopek@codesthq.com>
2015-12-22 11:23:46 +0100

    Level serializer

--- app/controllers/api/v1/levels_controller.rb
+++ app/controllers/api/v1/levels_controller.rb
@@ -1,5 +1,13 @@
 class API::V1::LevelsController < API::V1::BaseController
+  before_action :set_level
+
   def index
-    render json: { message: "Another" }, status: 200
+    render LevelSerializer.new(companies, API::V1::CompanyExtendedSerializer, root: "level", meta: true), status: :ok
+  end
+
+  private
+
+  def set_level
+
   end
 end
