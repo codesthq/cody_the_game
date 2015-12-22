1a41ede452fc311e71b13c7e7053a8a78aedffab
Radosław Bułat <radek.bulat@gmail.com>
2015-12-22 14:26:14 +0100

    Add equal-always-returns-boolean-really challenge

---
+++ challenges/equal-always-returns-boolean-really/challenge.rb
@@ -0,0 +1,11 @@
+# Challenge: write such a method 'change_object' to make the code work properly
+
+object = Object.new
+change_object(object)
+
+def empty?(o)
+  o.size == 0
+end
+
+puts empty?(o) # should print "Hello World" string
+
