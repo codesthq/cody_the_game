9d66b6eff1995e143051d71f8e406606cf8a4140
Tomasz Kowalewski <me@tkowalewski.pl>
2016-01-22 13:29:44 +0100

    Rewrite APIClient to TypeScript

---
+++ app/assets/javascripts/services/api_client.js.ts
@@ -0,0 +1,44 @@
+/// <reference path='../jquery.d.ts'/>
+
+interface ErrorCallback { (): void };
+interface SuccessCallback { (data: any): void };
+
+class APIClient{
+  ENDPOINT: string;
+
+  constructor(endpoint: string) {
+    this.ENDPOINT = endpoint;
+  }
+
+  listLevels(callback: SuccessCallback, error: ErrorCallback) {
+    var path = [this.ENDPOINT, "levels"].join("/");
+
+    $.get(path)
+      .done(callback)
+      .fail(error);
+  }
+
+  getLevel(id: number, callback: SuccessCallback, error: ErrorCallback) {
+    var path = [this.ENDPOINT, "levels", id].join("/");
+
+    $.get(path)
+      .done(callback)
+      .fail(error);
+  }
+
+  submitCode(level_id: number, content: string, callback: SuccessCallback, error: ErrorCallback) {
+    var path = [this.ENDPOINT, "submissions"].join("/");
+
+    $.post(path, { submission: { content: content, level_id: level_id } })
+      .done(callback)
+      .fail(error);
+  }
+
+  getSubmission(id: number, callback: SuccessCallback, error: ErrorCallback) {
+    var path = [this.ENDPOINT, "submissions", id].join("/");
+
+    $.get(path)
+      .done(callback)
+      .fail(error);
+  }
+}
