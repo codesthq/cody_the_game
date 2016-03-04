27d5fb82758c909ba84193fad57a93c0ceb07aa3
Michał Krajewski <contact@mkrajewski.net>
2016-03-04 13:24:15 +0100

    Update ruby_love.rb

--- app/challenges/challenge/ruby_love.rb
+++ app/challenges/challenge/ruby_love.rb
@@ -1,12 +1,10 @@
-# Challenge
-
-# Here is the funny method I wrote once:
+# There is "ruby_love" method:
 #
 # def ruby_love
 #   We ♥ Ruby! What about you?
 # end
 #
-# This method should return "I ♥ Ruby too!" string. You can't change 'ruby_love' method at all.
+# Write a code to return from this method "I ♥ Ruby too!" string without changing it.
 
 module Challenge
   class RubyLove < TaskRunner::BaseTask
