38eb70ad54e71f42128c954b51d0cf9204a5d0ab
Jarek Pelczynski <jpelczynski@gmail.com>
2016-03-04 15:31:09 +0100

    Add cover image to assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,3 +11,5 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
 # Rails.application.config.assets.precompile += %w()
+
+Rails.application.config.assets.precompile += %w(cover.png)
