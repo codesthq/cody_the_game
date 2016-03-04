646702396455f895f6ae5268ef60b08fa869e8f0
Bartosz Zurkowski <bartosz.zurkowski@codesthq.com>
2016-03-04 12:38:49 +0100

    Implement LevelUpdater service

---
+++ app/services/level_updater.rb
@@ -0,0 +1,51 @@
+class LevelUpdater
+  attr_reader :submission
+
+  def initialize(submission)
+    @submission = submission
+  end
+
+  def update!
+    if perform_update?
+      game_session.update max_level: succeeding_level
+    end
+  end
+
+  private
+
+  def perform_update?
+    level_authorized? && submission_succeed? && succeeding_level?
+  end
+
+  def level_authorized?
+    level_authorizer.authorized? level_number
+  end
+
+  def submission_succeed?
+    submission.succeed?
+  end
+
+  def succeeding_level?
+    succeeding_level > game_session.max_level
+  end
+
+  def level_authorizer
+    @level_authorizer ||= ::LevelAuthorizer.new game_session
+  end
+
+  def game_session
+    @game_session ||= submission.game_session
+  end
+
+  def succeeding_level
+    level_number + 1
+  end
+
+  def level_number
+    level.position
+  end
+
+  def level
+    @level ||= submission.level
+  end
+end
