59c8e9ffb26e0e2ff864712a5bcb7d90268e3802
Tomasz Kowalewski <me@tkowalewski.pl>
2016-07-20 21:10:02 +0200

    Set a timeout for ajax requests

--- app/assets/javascripts/services/api_client.js.ts
+++ app/assets/javascripts/services/api_client.js.ts
@@ -13,39 +13,51 @@ class APIClient{
   listLevels(callback: SuccessCallback, error: ErrorCallback) {
     var path = [this.ENDPOINT, "levels"].join("/");
 
-    $.get(path)
-      .done(callback)
-      .fail(error);
+    this.get(path, callback, error);
   }
 
   getLevel(id: number, callback: SuccessCallback, error: ErrorCallback) {
     var path = [this.ENDPOINT, "levels", id].join("/");
 
-    $.get(path)
-      .done(callback)
-      .fail(error);
+    this.get(path, callback, error);
   }
 
   getGameSession(callback: SuccessCallback, error: ErrorCallback) {
     var path = [this.ENDPOINT, "game_session"].join("/");
 
-    $.get(path)
-        .done(callback)
-        .fail(error);
+    this.get(path, callback, error);
   }
 
   submitCode(level_id: number, content: string, callback: SuccessCallback, error: ErrorCallback) {
     var path = [this.ENDPOINT, "submissions"].join("/");
+    var data = { submission: { content: content, level_id: level_id } };
 
-    $.post(path, { submission: { content: content, level_id: level_id } })
-      .done(callback)
-      .fail(error);
+    this.post(path, data, callback, error);
   }
 
   getSubmission(id: number, callback: SuccessCallback, error: ErrorCallback) {
     var path = [this.ENDPOINT, "submissions", id].join("/");
 
-    $.get(path)
+    this.get(path, callback, error);
+  }
+
+  private get(path: string, callback: SuccessCallback, error: ErrorCallback) {
+    $.ajax({
+        type: 'GET',
+        url: path,
+        timeout: 1000
+      })
+      .done(callback)
+      .fail(error);
+  }
+
+  private post(path: string, data: any, callback: SuccessCallback, error: ErrorCallback) {
+    $.ajax({
+        type: 'POST',
+        url: path,
+        data: data,
+        timeout: 1000
+      })
       .done(callback)
       .fail(error);
   }
